5aa9d9588b3a81657fdbe406d760537d43efb7c3
haji <s1230149@gmail.com>
2018-11-15 01:09:43 +0900

    use template type

--- SparseTable.cpp
+++ SparseTable.cpp
@@ -10,9 +10,9 @@ using namespace std;
   
   区間-(操作名) [option] 計算量(build)
   
-  区間-(gcd) [0] O( N log(N) log(N) ) 
-  区間-(min) [1] O( N log(N) )
-  区間-(max) [2] O( N log(N) )
+  区間-(min) [0] O( N log(N) )
+  区間-(max) [1] O( N log(N) )
+  区間-(gcd) [2] O( N log(N) log(N) ) 
   
   [get]
   gcd -> O( log(N) )
@@ -27,43 +27,46 @@ using namespace std;
   
  */
 
+template<typename T>
 class SparseTable{
 public:
-  typedef long long ll;
-  
-  int LOG = 20;
-  ll N, option; // option : 0 -> gcd, 1 -> min, 2 -> max
-  vector<vector<ll> > Array;
-  vector<ll> Memo, Log;
+  int LOG;
+  int N, option; // option : 0 -> min, 1 -> max, 2 -> gcd
+  vector<vector<T> > Array;
+  vector<int> Memo, Log;
   int ok = 0;
   SparseTable(){}
   
-  SparseTable(ll N, ll option): 
-    N(N), option(option), Array(LOG,vector<ll>(N)), Memo(N+1), Log(N+1){ 
-    set<ll> pepi;
-    for(ll i=0;i<LOG;i++){
-      ll x = 1<<i;
+  SparseTable(int N, int option): 
+    N(N), option(option), Memo(N+1), Log(N+1){
+    LOG = 0;
+    while(N >= (1<<LOG)) LOG++;
+    Array = vector<vector<T> >(LOG,vector<T>(N));
+    
+    vector<int> num(LOG);
+    for(int i=0;i<LOG;i++){
+      int x = 1<<i;
       if( x <= N ) Log[x] = i;
-      pepi.insert(x);
+      num[i] = x;
     }
-    for(ll i=1;i<=N;i++) Memo[i] = *pepi.lower_bound( i + 1 ) >> 1;
+    for(int i=1;i<=N;i++) Memo[i] = *lower_bound(num.begin(), num.end(), ( i + 1 ) >> 1);
   }
   
   // k番目の値をvalにする。 ( 0 origin )
-  void update(ll k, ll val){assert(0<=k && k < N); ok = 0; Array[0][k] = val; }
+  void update(int k, T val){assert(0<=k && k < N); ok = 0; Array[0][k] = val; }
   
-  ll merge(ll A, ll B){
-    if( option == 0 ) return __gcd( A, B );
-    if( option == 1 ) return min( A, B );
-    if( option == 2 ) return max( A, B );
+  inline T merge(const T &A,const T &B){
+    if( option == 0 ) return min( A, B );
+    if( option == 1 ) return max( A, B );
+    //if( option == 2 ) return __gcd( A, B );
     assert(0);
   }
   
   void build(){
     ok = 1;
-    for(ll i=1;i<LOG;i++){
-      ll len = 1<<i;
-      for(ll j=0;j<N;j++){
+    for(int i=1;i<LOG;i++){
+      int len = 1<<i;
+      for(int j=0;j<N;j++){
         if( N <= j + len - 1 ) continue;
         Array[i][j] = merge( Array[i-1][j], Array[i-1][j+len/2] );
       }
@@ -71,10 +74,10 @@ public:
   }
   
   // [ l, r )の [optionを満たす値] を返す ( 0 origin )
-  ll get(ll l, ll r){
+  T get(int l, int r){
     assert(ok);
     assert(l < r && 0 <= l && r <= N);
-    ll k = Memo[r-l];
+    int k = Memo[r-l];
     return merge( Array[Log[k]][l], Array[Log[k]][r-k] );
   }
 };
@@ -83,8 +86,7 @@ signed main(){
   
   int n;
   cin>>n;
-  
-  SparseTable A( n, 0 );
+  SparseTable<long long> A( n, 0 );
   
   for(int i=0;i<n;i++){
     int a;
