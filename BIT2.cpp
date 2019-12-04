082d23993deab7fd5b2a8beae12bd808b78d6acf
haji <s1230149@gmail.com>
2019-12-05 05:46:26 +0900

    refactoring

--- BIT2.cpp
+++ BIT2.cpp
@@ -3,43 +3,42 @@ using namespace std;
 
 //区間加算、区間和, 0オリジン
 
+template<typename D = long long, int useAssert = 1>
 class BIT{
 public:
-  typedef long long ll;
-  ll n;
-  vector<ll> bit0, bit1;
-  BIT(){n=-1;}
-  BIT(ll n_){
-    n=1;
-    while(n<n_)n*=2;
-    bit0.resize(n+1,0);
-    bit1.resize(n+1,0);
+  int n;
+  vector<D> bit0, bit1;
+  BIT(){n = -1;}
+  BIT(int n_){
+    n = 1;
+    while(n < n_) n *= 2;
+    bit0.resize(n+1, 0);
+    bit1.resize(n+1, 0);
   }
-  
-  ll sum(vector<ll> &b,ll i){
-    ll s=0;
-    while(i>0){
+
+  D sum(vector<D> &b, int i){
+    D s = 0;
+    while(i > 0){
       s = (s + b[i]);// % mod;
       i -= i & -i;
     }
     return s;
   }
 
-  void add(vector<ll> &b,ll i,ll v){
+  void add(vector<D> &b, int i, D v){
     // v = (mod + v % mod) % mod;
-    
-    while(i<=n){
+    while(i <= n){
       b[i] = (b[i] + v);//% mod;
       i += i & -i;
     }
   }
 
   //[a,b)にxを加算 0オリジン
-  void add (ll a,ll b,ll x){
+  void add (int a, int b, D x){
     // x = (mod + x % mod) % mod;
     if(a == b) return;
     a++;
-    assert(a <= b && 1 <= a && b <= n);
+    if(useAssert) assert(a <= b && 1 <= a && b <= n);
     add(bit0, a, -x*(a-1));
     add(bit1, a, x);
     add(bit0, b+1, x*b);
@@ -47,26 +46,25 @@ public:
   }
 
   //[a,b)のsumを得る 0オリジン
-  ll sum(ll a,ll b){
+  D sum(int a, int b){
     if(a == b) return 0;
     a++;
-    assert(a <= b && 1 <= a && b <= n);
-    ll s = sum(bit0,b);
-    ll t = sum(bit1,b) * b;
-    ll u = sum(bit0,a-1);
-    ll v = sum(bit1,a-1) * (a-1); 
+    if(useAssert) assert(a <= b && 1 <= a && b <= n);
+    D s = sum(bit0, b);
+    D t = sum(bit1, b) * b;
+    D u = sum(bit0, a-1);
+    D v = sum(bit1, a-1) * (a-1);
     return s + t - u - v;
     //return ((s + t) + (mod - u%mod) + (mod - v%mod)) % mod;
   }
 };
 
-
 int main(){
   int n, q;
   cin>>n>>q;
 
-  BIT bit(n);
-  
+  BIT <> bit(n);
+
   for(int i=0;i<q;i++){
     int cmd;
     cin>>cmd;
@@ -82,5 +80,5 @@ int main(){
       cout<<ans<<endl;
     }
   }
-  
+
 }
