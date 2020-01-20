3f88443c59a5e601fed8cc6b2c26e22465722657
haji <s1230149@gmail.com>
2020-01-21 07:22:41 +0900

    initialize ok=false, T getL(T x) -> int getL(T x)

--- Compress.cpp
+++ Compress.cpp
@@ -8,21 +8,21 @@ class Compress{
 public:
   vector<T> X;
   bool ok;
-  Compress(){};
-  
-  int size(){return X.size();}
+  Compress():ok(false){};
+
+  int size(){assert(ok); return X.size();}
   void add(T a){ok = false; X.push_back(a); X.push_back(a-1); X.push_back(a+1);}
   void add(const vector<T> a){for(T x:a) add(x);}
   //void add(const vector<T> &a){for(T x:a) add(x);}
-  
+
   void build(){
     ok = true;
     sort(X.begin(),X.end());
     X.erase(unique(X.begin(),X.end()),X.end());
   }
-  
-  T getL(T x){assert(ok); return lower_bound(X.begin(),X.end(),x) - X.begin();}
-  T getU(T x){assert(ok); return upper_bound(X.begin(),X.end(),x) - X.begin();}
+
+  int getL(T x){assert(ok); return lower_bound(X.begin(),X.end(),x) - X.begin();}
+  int getU(T x){assert(ok); return upper_bound(X.begin(),X.end(),x) - X.begin();}
 };
 
 
